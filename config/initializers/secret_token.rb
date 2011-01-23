1110ff9457033fd2af943c502c7c75576af12c57
firebird-gda <firebird.gda@hotmail.com>
2011-01-23 19:38:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8af64ff0ab8f92bcdd3b0eb614f759326f745142c5356f0a05c1d60c703ad1b5a7028eb4e9c9f25a2c361829dcff5ffeda2ca5ee169214d2a3bb9725b497b191'
